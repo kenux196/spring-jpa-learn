8c7ca730acd022602eeae97fb9b3c69d49cd6f70
skyun <skyun@a2tec.co.kr>
2021-02-06 12:20:58 +0900

    상품 주문 서비스 구현 및 테스트 코드 작성

---
+++ usingBootAndJPA/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,64 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.*;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final ItemRepository itemRepository;
+
+    /**
+     * 주문 생성
+     * @param memberId member id
+     * @param itemId 아이템 Id
+     * @param count 주문 수량
+     * @return order id
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.READY);
+
+        // 주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문생성
+        Order order = Order.createOrder(member,delivery, orderItem);
+
+        // 주문저장
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    /**
+     * Cancel order
+     * @param orderId order id
+     */
+    @Transactional
+    public void cancel(Long orderId) {
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        // 주문 취소
+        order.cancel();;
+    }
+
+    // 주문 검색
+}
